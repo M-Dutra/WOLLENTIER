f4712a6b8eb3310995113193eff4bdda4be964f2
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-03-07 18:08:38 +0100

    Test User Profile - create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @restaurant.save
+    # When creates, we can show the final profile, to see the final result!
+    redirect_to users_path(@users)
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name,
+                                 :last_name,
+                                 :phone_number,
+                                 :interests)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
