f70426bd8c294bbd1e5426e2063f7f7c25a7c681
Manuel Dutra <manuel.f.dutra8@gmail.com>
2023-03-07 15:45:33 +0100

    models and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_one :organization
   has_many :applications
+  enum :role, [:volunteer, :organization]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_133229) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_143907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_133229) do
     t.bigint "organization_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "district"
     t.index ["organization_id"], name: "index_offers_on_organization_id"
   end
 
