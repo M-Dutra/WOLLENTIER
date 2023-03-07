5331f98ff9a65228a2bfc92e272565fcc3507fe4
tulio bonilla <tuliobonilla@gmail.com>
2023-03-07 15:47:00 +0100

    removed empty line

--- db/schema.rb
+++ db/schema.rb
@@ -25,5 +25,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_143656) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
