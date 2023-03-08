0315a8311ed65a451e9ee28565ace78c0280f99a
Jonas Ajubi <jonas+github@giantmonkey.de>
2023-03-08 11:48:02 +0100

    updated controller and views

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,2 +1,1 @@
-class OffersController < ApplicationController
-end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'index', to: 'offers#index'
+  get 'home', to: 'pages#home'
 end
